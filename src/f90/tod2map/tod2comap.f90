b555e5383c383451928cdda80536836612b9a164
Marie Kristine Foss <m.k.foss@astro.uio.no>
2017-04-19 16:09:28 +0200

    A start

--- src/f90/tod2map/tod2comap.f90
+++ src/f90/tod2map/tod2comap.f90
@@ -5,5 +5,37 @@ program tod2comap
   use tod2comap_cl_mod
   implicit none
 
+!  include "mpif.h"
+
+!  integer(i4b)  :: myid, numprocs, ierr, root
+
+  type map_type
+     integer(i4b) :: n_x, n_y, numfreq, n_k
+     real(dp)     :: dtheta
+     real(dp), allocatable, dimension(:)     :: x, y, f, k ! (n_x or n_y or numfreq or n_k)
+     real(dp), allocatable, dimension(:,:,:) :: m, rms, dsum, nhit, div ! (n_x, n_y, numfreq)
+  end type map_type
+
+!  call mpi_init(ierr)
+!  call mpi_comm_rank(mpi_comm_world, myid, ierr)
+!  call mpi_comm_size(mpi_comm_world, numprocs, ierr)
+!  root = 0
+
+  type(map_type) :: map
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+!  call mpi_finalize(ierr)
 
 end program tod2comap
