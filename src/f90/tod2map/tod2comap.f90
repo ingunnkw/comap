afdfd3cb7e53191d858037318b405e43ca5547e3
Marie Kristine Foss <m.k.foss@astro.uio.no>
2017-04-20 15:10:17 +0200

    Small changes

--- src/f90/tod2map/tod2comap.f90
+++ src/f90/tod2map/tod2comap.f90
@@ -1,13 +1,19 @@
 program tod2comap
   use comap_lx_mod
-  use tod2comap_utils
-  use tod2comap_mapmaker
-  use tod2comap_cl_mod
   implicit none
 
 !  include "mpif.h"
 
 !  integer(i4b)  :: myid, numprocs, ierr, root
+
+  type tod_type ! ???
+     real(dp)     :: samprate, Tsys
+     integer(i4b) :: numsamp, numdet, numfreq
+     real(dp)     :: fmin, fmax, df
+     real(dp), allocatable, dimension(:)     :: t, f             ! (time or freq)
+     real(dp), allocatable, dimension(:,:,:) :: d, d_raw, g, rms ! (time, freq, det) 
+     real(dp), allocatable, dimension(:,:,:) :: point            ! (time, 3, numdet)
+  end type tod_type
 
   type map_type
      integer(i4b) :: n_x, n_y, numfreq, n_k
@@ -16,26 +22,30 @@ program tod2comap
      real(dp), allocatable, dimension(:,:,:) :: m, rms, dsum, nhit, div ! (n_x, n_y, numfreq)
   end type map_type
 
+  type(tod_type)  :: tod
+  type(map_type)  :: map
+  type(lx_struct) :: data
+
+  character(len=512) :: filename
+  integer(i4b) :: i, j, k
+
 !  call mpi_init(ierr)
 !  call mpi_comm_rank(mpi_comm_world, myid, ierr)
 !  call mpi_comm_size(mpi_comm_world, numprocs, ierr)
 !  root = 0
-
-  type(map_type) :: map
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 !  call mpi_finalize(ierr)
 
+  ! Read data
+  call read_l3_file(filename, data)
+
+
+contains
+
+  subroutine compute_maps(data, map)
+    implicit none
+
+
+  end subroutine compute_maps
+
+ 
 end program tod2comap
