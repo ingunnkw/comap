e0d55a51f0594321c7864b516d8b8a035ffd9ef6
Marie Kristine Foss <m.k.foss@astro.uio.no>
2017-05-08 14:46:44 +0200

    tod2comap kind of works

--- src/f90/tod2map/tod2comap.f90
+++ src/f90/tod2map/tod2comap.f90
@@ -1,53 +1,273 @@
 program tod2comap
   use comap_lx_mod
+  !use comap_ces_mod
+  use quiet_fft_mod
   implicit none
 
 !  include "mpif.h"
 
-  ! type tod_type ! ???
-  !    real(dp)     :: samprate, Tsys
-  !    integer(i4b) :: numsamp, numdet, numfreq
-  !    real(dp)     :: fmin, fmax, df
-  !    real(dp), allocatable, dimension(:)     :: t, f             ! (time or freq)
-  !    real(dp), allocatable, dimension(:,:,:) :: d, d_raw, g, rms ! (time, freq, det) 
-  !    real(dp), allocatable, dimension(:,:,:) :: point            ! (time, 3, numdet)
-  ! end type tod_type
+  type tod_type
+     real(dp)     :: samprate, Tsys
+     integer(i4b) :: nsamp, ndet, nfreq
+     real(dp)     :: fmin, fmax, df
+     real(dp), allocatable, dimension(:)     :: t, f             ! (time or freq)
+     real(dp), allocatable, dimension(:,:,:) :: d, d_raw, g, rms ! (time, freq, det) 
+     real(dp), allocatable, dimension(:,:)   :: point            ! (3, time)
+  end type tod_type
 
-  ! type map_type
-  !    integer(i4b) :: n_x, n_y, numfreq, n_k
-  !    real(dp)     :: dtheta
-  !    real(dp), allocatable, dimension(:)     :: x, y, f, k ! (n_x or n_y or numfreq or n_k)
-  !    real(dp), allocatable, dimension(:,:,:) :: m, rms, dsum, nhit, div ! (n_x, n_y, numfreq)
-  ! end type map_type
+  type map_type
+     integer(i4b) :: n_x, n_y, nfreq, n_k
+     real(dp)     :: dtheta
+     real(dp), allocatable, dimension(:)     :: x, y, f, k ! (n_x or n_y or nfreq or n_k)
+     real(dp), allocatable, dimension(:,:,:) :: m, rms, dsum, nhit, div ! (n_x, n_y, nfreq)
+  end type map_type
 
-  ! type(tod_type)  :: tod
-  ! type(map_type)  :: map
+  type(tod_type)  :: tod
+  type(map_type)  :: map
   type(lx_struct) :: data
+  !type(comap_runlist)  :: runlist
 
   character(len=512) :: filename
-!  integer(i4b) :: i, j, k
-!  integer(i4b)  :: myid, numprocs, ierr, root
+  !integer(i4b) :: i, j, k
+  !integer(i4b)  :: myid, numprocs, ierr, root
 
-!  call mpi_init(ierr)
-!  call mpi_comm_rank(mpi_comm_world, myid, ierr)
-!  call mpi_comm_size(mpi_comm_world, numprocs, ierr)
-!  root = 0
-!  call mpi_finalize(ierr)
+  !call mpi_init(ierr)
+  !call mpi_comm_rank(mpi_comm_world, myid, ierr)
+  !call mpi_comm_size(mpi_comm_world, numprocs, ierr)
+  !root = 0
+  !call mpi_finalize(ierr)
 
   filename = "/mn/stornext/d5/comap/protodir/level3/Ka/lissajous/patch1_1.h5"
+  write(*,*) 'Get TOD ...'
+  call get_tod(filename, data, tod)
+  !write(*,*) 'Write TOD to file ...'
+  !call output_tod('files/test_tod.dat', 1, tod)
+  write(*,*) 'Compute maps ...'
+  call compute_maps(data, tod, map)
+  write(*,*) 'Write maps to file ...'
+  call output_maps('files/test', map)
+  write(*,*) 'Done'
 
-  ! Read data
-  call read_l3_file(filename, data)
+contains
 
-  write(*,*) "So far, so good"
+  subroutine hp_filter(nu_cut, tod_d)
+    implicit none
 
-!contains
+    integer(i4b),                intent(in)    :: nu_cut
+    real(dp),     dimension(1:), intent(inout) :: tod_d
 
-!  subroutine compute_maps(data, map)
-!    implicit none
+    integer(i4b) :: n
+    complex(dpc), allocatable, dimension(:) :: tod_fft
+
+    n = size(tod_d)/2 + 1
+    allocate(tod_fft(n))
+
+    call fft(tod_d, tod_fft, 1)
+    tod_fft(1:nu_cut) = 0.d0
+    call fft(tod_d, tod_fft, -1)
+    
+    deallocate(tod_fft)
+
+  end subroutine hp_filter
 
 
-!  end subroutine compute_maps
+  subroutine get_tod(l3file, data, tod)
+    implicit none
+    character(len=*), intent(in)    :: l3file
+    type(lx_struct),  intent(inout) :: data
+    type(tod_type),   intent(inout) :: tod
+
+    integer(i4b) :: i, j, k, nu_cut
+   
+    nu_cut = 10000
+
+    ! Read data
+    call read_l3_file(l3file, data)
+
+    tod%samprate = data%samprate
+    tod%nsamp = size(data%time)
+    tod%nfreq = size(data%nu)
+    tod%ndet  = size(data%tod,3)
+
+    allocate( tod%t(tod%nsamp), tod%f(tod%nfreq), &
+         & tod%point(3,tod%nsamp), &
+         & tod%d_raw(tod%nsamp, tod%nfreq, tod%ndet), &
+         & tod%d(tod%nsamp, tod%nfreq, tod%ndet), &
+         & tod%g(tod%nsamp, tod%nfreq, tod%ndet), &
+         & tod%rms(tod%nsamp, tod%nfreq, tod%ndet))
+
+    tod%t = data%time; tod%f = data%nu
+
+    do k = 1, tod%ndet
+       do j = 1, tod%nfreq
+          do i = 1, tod%nsamp
+            ! tod%d_raw(i,j,k) = mean
+             tod%d_raw(i,j,k) = data%tod(i,j,k) ! Does not seem to work?
+          end do
+          
+          ! Apply high pass filter
+          tod%d(:,j,k) = tod%d_raw(:,j,k)
+          call hp_filter(nu_cut, tod%d(:,j,k))
+
+          ! Estimate RMS
+          tod%rms(:,j,k) = sqrt(variance(tod%d(:,j,k)))
+       end do
+    end do
+
+  end subroutine get_tod
+
+
+  subroutine output_tod(filename, det, tod)
+    implicit none
+    character(len=*), intent(in) :: filename
+    type(tod_type),   intent(in) :: tod
+    integer(i4b),     intent(in) :: det
+
+    integer(i4b) :: unit, i, j
+
+    unit = getlun()
+
+    open(unit, file=trim(filename), recl=4096)
+    do i = 1, tod%nsamp
+       write(unit, fmt='(f16.8)', advance='no') tod%t(i)
+       do j = 1, tod%nfreq
+          write(unit, fmt='(2f2.8)', advance='no') tod%d(i,j,det), tod%d_raw(i,j,det)
+       end do
+       write(unit,*)
+    end do
+    close(unit)
+
+  end subroutine output_tod
+  
+
+  subroutine compute_maps(data, tod, map)
+    implicit none
+    type(lx_struct), intent(in)    :: data
+    type(tod_type),  intent(in)    :: tod
+    type(map_type),  intent(inout) :: map
+    
+    integer(i4b) :: i, j, k, p, q
+    real(dp)     :: x_min, x_max, y_min, y_max, pad
+
+    ! Set up map grid
+    pad = 0.3d0 ! degrees
+    map%nfreq = tod%nfreq
+    map%dtheta = 5.d0/60.d0 ! Arcmin
+    x_min = data%point_lim(1) - pad; x_max = data%point_lim(2) + pad
+    y_min = data%point_lim(3) - pad; y_max = data%point_lim(4) + pad
+    map%n_x = (x_max-x_min)/map%dtheta+1; map%n_y = (x_max-x_min)/map%dtheta+1
+    allocate(map%x(map%n_x), map%y(map%n_y))
+    do i = 1, map%n_x
+       map%x(i) = x_min + (i-1)*map%dtheta
+    end do
+    do i = 1, map%n_y
+       map%y(i) = y_min + (i-1)*map%dtheta
+    end do
+
+    ! Set up map structures
+    if (.not. allocated(map%dsum)) then
+       allocate(map%m(map%n_x, map%n_y, map%nfreq), map%dsum(map%n_x, map%n_y, map%nfreq), &
+            & map%nhit(map%n_x, map%n_y, map%nfreq), map%div(map%n_x, map%n_y, map%nfreq), &
+            & map%rms(map%n_x, map%n_y, map%nfreq))
+       map%dsum = 0.d0
+       map%nhit = 0.d0
+       map%div  = 0.d0
+    end if
+
+    ! Co-add into maps
+    do k = 1, tod%ndet
+       do i = 1, tod%nsamp
+          p = min(max(int((tod%point(1,i)-x_min)/map%dtheta),1),map%n_x)
+          q = min(max(int((tod%point(2,i)-y_min)/map%dtheta),1),map%n_y)
+          do j = 1, tod%nfreq
+             map%dsum(p,q,j) = map%dsum(p,q,j) + tod%g(i,j,k)    / tod%rms(i,j,k)**2 * tod%d(i,j,k)
+             map%div(p,q,j)  = map%div(p,q,j)  + tod%g(i,j,k)**2 / tod%rms(i,j,k)**2
+             map%nhit(p,q,j) = map%nhit(p,q,j) + 1.d0
+          end do
+       end do
+    end do
+    where(map%nhit > 0)
+       map%m   = map%dsum / map%div
+       map%rms = 1.d0 / sqrt(map%div)
+    elsewhere
+       map%m   = 0.d0
+       map%rms = 0.d0
+    end where
+
+!!$    ! Report reduced chisquares
+!!$    do i = 1, map%nfreq
+!!$       nu    = count(map%nhit(:,:,i)>0)
+!!$       chisq = sum((map%m(:,:,i)/map%rms(:,:,i))**2,map%nhit(:,:,i)>0)
+!!$       write(*,fmt='(a,i5,a,f8.3,a,f8.3)') 'Freq = ', i, ', red chisq = ', chisq/nu, ', sigma = ', (chisq-nu)/sqrt(2.d0*nu)
+!!$    end do
+
+  end subroutine compute_maps
+
+
+  subroutine output_maps(prefix, map)
+    implicit none
+    character(len=*), intent(in) :: prefix
+    type(map_type),   intent(in) :: map
+
+    integer(i4b)       :: i, j, k, unit
+    character(len=4)   :: itext
+    character(len=512) :: filename
+
+    unit = getlun()
+    do i = 1, map%nfreq
+       call int2string(i,itext)
+       filename = trim(prefix)//'_freq'//itext//'_map.dat'
+       open(unit, file=trim(filename), recl=100000)
+       write(unit,*) '# n_x = ', map%n_x
+       write(unit,*) '# n_y = ', map%n_y
+       write(unit,*) '# x   = ', real(map%x,sp)
+       write(unit,*) '# y   = ', real(map%y,sp)
+       do j = 1, map%n_x
+          do k = 1, map%n_y
+             write(unit,fmt='(f16.8)',advance='no') map%m(j,k,i)
+          end do
+          write(unit,*)
+       end do
+       close(unit)
+    end do
+
+    unit = getlun()
+    do i = 1, map%nfreq
+       call int2string(i,itext)
+       filename = trim(prefix)//'_freq'//itext//'_rms.dat'
+       open(unit, file=trim(filename), recl=100000)
+       write(unit,*) '# n_x = ', map%n_x
+       write(unit,*) '# n_y = ', map%n_y
+       write(unit,*) '# x   = ', real(map%x,sp)
+       write(unit,*) '# y   = ', real(map%y,sp)
+       do j = 1, map%n_x
+          do k = 1, map%n_y
+             write(unit,fmt='(f16.8)',advance='no') map%rms(j,k,i)
+          end do
+          write(unit,*)
+       end do
+       close(unit)
+    end do
+
+    unit = getlun()
+    do i = 1, map%nfreq
+       call int2string(i,itext)
+       filename = trim(prefix)//'_freq'//itext//'_nhit.dat'
+       open(unit, file=trim(filename), recl=100000)
+       write(unit,*) '# n_x = ', map%n_x
+       write(unit,*) '# n_y = ', map%n_y
+       write(unit,*) '# x   = ', real(map%x,sp)
+       write(unit,*) '# y   = ', real(map%y,sp)
+       do j = 1, map%n_x
+          do k = 1, map%n_y
+             write(unit,fmt='(f16.8)',advance='no') map%nhit(j,k,i)
+          end do
+          write(unit,*)
+       end do
+       close(unit)
+    end do
+
+  end subroutine output_maps
 
  
 end program tod2comap
