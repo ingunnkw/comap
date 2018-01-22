f32f91ca95168461f8070ec24b8c01d76d14c4ff
Ingunn <i.k.wehus@fys.uio.no>
2018-01-22 16:25:39 +0100

    first version of gain mod

--- src/f90/include/comap_gain_mod.f90
+++ src/f90/include/comap_gain_mod.f90
@@ -15,22 +15,22 @@ contains
     initialized = .true.
   end subroutine
 
-  subroutine estimate_gain(el,dat,g,sigma0)
+  subroutine estimate_gain(el,dat,g,sigma0,chisq)
     implicit none
     real(dp), dimension(:)                :: el, dat
-    real(dp), dimension(:),   optional    :: sigma0
+    real(dp),                 optional    :: sigma0, chisq
     real(dp),                 intent(out) :: g
     real(dp), dimension(:,:), allocatable :: templ, data, mat, hm
     real(dp), dimension(:),   allocatable :: amp
-    integer(i4b)                          :: n, npol, nt, i, j 
-    real(dp)                              :: chisq
+    integer(i4b)                          :: n, npt, nt, i, j 
 
-    npol = 3
-    nt = 1 + npol
+    npt = 2
+    nt = 1 + npt
     n = size(el)
     allocate(templ(n,nt), data(n,1), amp(nt), mat(nt,nt), hm(nt,1))
     templ(:,1) = 1/(sin(el*pi/180.))
-    do j = 1, npol
+    do j = 1, npt
+       write(*,*) j, 'inside polytemp loop'
        do i = 1, n
           templ(i,j+1) = (real(i,dp)-(real(n,dp)/2.))**(j-1)
        end do
@@ -40,12 +40,18 @@ contains
     hm =  matmul(transpose(templ), data)
     call  solve_linear_system_dp(mat, amp, hm(:,1))
     g = amp(1)
+    write(*,*) g/1d10, '=gain'
 
-!    if (exists(sigma0)) then
-!       hm(:,1) = amp
-!       data = (data -matmul(templ,hm))/sigma0
-!       chisq = sum(data(:,1))
-!    end if
+!    g = sum(dat*templ(:,1))/sum(templ(:,1)*templ(:,1))
+!    write(*,*) g/1d10, '=gain'
+
+
+    if (present(sigma0) .and. present(chisq)) then
+       hm(:,1) = amp
+       data = (data -matmul(templ,hm))/sigma0
+       chisq = sum((data(:,1))**2)
+       !write(*,*) chisq, '= chisq'
+    end if
 
   end subroutine estimate_gain
 
